450819e93f9199c0d8d98f3cdfc1cce1220bcd0b
Navya140311 <72936140+Navya140311@users.noreply.github.com>
2021-02-17 15:11:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('magnificent_moustache_img.png');
+}
